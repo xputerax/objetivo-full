7798645332740452d5475eeb4874ea5667b7de91
Aiman Daniel <aiman2301@gmail.com>
2022-05-12 17:40:58 +0800

    Add jQuery to base template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
     <title>{{ config('app.name', 'Objetivo') }} / @yield('title')</title>
 
     @include('layouts._styles')
+
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
