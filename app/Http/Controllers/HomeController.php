35df0d8fe7803c5ab2483a45b59f34c531d23c02
Aiman Daniel <aiman2301@gmail.com>
2022-05-12 14:46:54 +0800

    Show actual goals count by status on dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\DashboardServiceInterface;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    private $dashboardService;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(DashboardServiceInterface $dashboardService)
     {
         $this->middleware('auth');
+        $this->dashboardService = $dashboardService;
     }
 
     /**
      * Show the application dashboard.
      *
+     * @param Request $request
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $this->dashboardService->forUser($request->user());
+
+        $data = [
+            'not_started_count' => $this->dashboardService->getNotStartedCount(),
+            'in_progress_count' => $this->dashboardService->getInProgressCount(),
+            'completed_count' => $this->dashboardService->getCompletedCount(),
+        ];
+
+        return view('home', $data);
     }
 }
