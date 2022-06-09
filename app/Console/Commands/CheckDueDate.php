b36d5fffbbb402c5f73cac0ad95040dd2789269c
Daniel Wan Jonathan <daniel01.jonathan@gmail.com>
2022-06-09 22:12:10 +0800

    checkduedate artisan command now will return the interval from current time to due date time

--- app/Console/Commands/CheckDueDate.php
+++ app/Console/Commands/CheckDueDate.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Goal;
+use DateTimeZone;
 
 class CheckDueDate extends Command
 {
@@ -27,6 +29,28 @@ class CheckDueDate extends Command
      */
     public function handle()
     {
-        echo 'This is my first basic scheduler';
+        //Get id, user_id and due date from database for all users
+        //Returns json
+        $data = Goal::select('id','user_id','due_at')->get();
+
+        //Decode the json file
+        $datarr = json_decode($data, true);
+        
+        //Calculate the interval(in hours) from current time to due date for each goal in the database
+        date_default_timezone_set('Asia/Kuala_Lumpur');
+        $timeformat = 'Y-m-d H:i:s';
+        foreach ($datarr as $item) {
+            //Date in Date object
+            $duedate = date($timeformat,strtotime($item['due_at']));
+            $currdate = date($timeformat);
+
+            //Calculate difference in hours
+            $hours = abs(strtotime($duedate) - strtotime($currdate))/3600;
+            $hours += 8; //Correct the timezone
+            $hours = (int)$hours; //Convert to integer
+
+            echo "Goal ID: ".$item['id']."|| ".$duedate." ||".$hours." hours\n";
+        }
+        
     }
 }
