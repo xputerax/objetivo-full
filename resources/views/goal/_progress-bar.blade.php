58462a5b8d3902b2e8bc1bf8fa6bb9286a43d3b7
alifmazli <alifmazli01@gmail.com>
2022-06-13 00:52:35 +0800

    Replaced progress bar and changed page title

--- resources/views/goal/_progress-bar.blade.php
+++ resources/views/goal/_progress-bar.blade.php
@@ -1,11 +1,37 @@
-<!-- Progress Bar -->
+{{-- <!-- Progress Bar -->
 <section id="progress-bar-section" class="progress-bar-section">
     <div class="progress-bar-title">Progress Bar</div>
     <div class="progress">
         <span class="progress-bar bg-success"
             id="progress-bar-id" role="progressbar"
-            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
         </span>
     </div> <!-- / .progress -->
 </section>
-<!-- End Progress Bar -->
+<!-- End Progress Bar --> --}}
+
+<section id="progress-bar-section" class="progress-bar-section">
+    <div class="progress-bar-title">Progress Bar</div>
+    <div class="progress">
+        <div class="container">
+            <div class="progress progress-striped">
+                <div class="progress-bar">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="progress2 progress-moved">
+                <div class="progress-bar2">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="progress progress-infinite">
+                <div class="progress-bar3">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
