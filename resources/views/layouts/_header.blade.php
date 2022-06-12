81e11b7b313d0e06da5363c72e0bdb98a9052e9c
Daniel Wan Jonathan <daniel01.jonathan@gmail.com>
2022-06-12 20:35:48 +0800

    Remove the link for notification bell from navbar

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,6 @@
 
     <nav class="header-nav ms-auto">
         <ul class="d-flex align-items-center">
-            @include('layouts._navbar-notification-bell')
             @include('layouts._navbar-profile')
         </ul>
     </nav><!-- End Icons Navigation -->
