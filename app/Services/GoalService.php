14ed31a6f7c6d63c3f32f28425a2342506d11a91
Daniel Wan Jonathan <daniel01.jonathan@gmail.com>
2022-06-17 14:00:35 +0800

    Modified query

--- app/Services/GoalService.php
+++ app/Services/GoalService.php
@@ -25,7 +25,7 @@ class GoalService implements GoalServiceInterface
     public function getGoals()
     {
         $goals = Goal::select('id','user_id','title','description','smart_goal','due_at','created_at','updated_at')
-            ->where('user_id',2)  //Use $this->user->id for current user
+            ->where('user_id',$this->user->id)
             ->get();
         
         return $goals;
