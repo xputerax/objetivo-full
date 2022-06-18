84a8f00ffe3a20eefb0a4c6fe5221cca1df8e912
alifmazli <alifmazli01@gmail.com>
2022-06-18 18:26:42 +0800

    Removed extra progress bar

--- resources/views/goal/_progress-bar.blade.php
+++ resources/views/goal/_progress-bar.blade.php
@@ -13,32 +13,6 @@
 <section id="progress-bar-section" class="progress-bar-section">
     <div class="progress-bar-title">Progress Bar</div>
     <div class="progress">
-        {{-- <div class="container">
-            <div class="progress progress-striped">
-                <div class="progress-bar">
-                </div>
-            </div>
-        </div>
-
-        <div class="container">
-            <div class="progress2 progress-moved">
-                <div class="progress-bar2">
-                </div>
-            </div>
-        </div> --}}
-
-        <div class="container">
-            <div class="progress progress-infinite">
-                <div class="progress-bar3">
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="progress-bar-section" class="progress-bar-section">
-    <div class="progress-bar-title">Progress Bar</div>
-    <div class="progress">
         <span class="progress-bar bg-success" id="progress-bar-id" role="progressbar" aria-valuenow="50"
             aria-valuemin="0" aria-valuemax="100">
         </span>
