9dbb9529582c08a93d33fa77f2d13caf59c95b66
alifmazli <alifmazli01@gmail.com>
2022-06-20 00:05:10 +0800

    Minor reformatting

--- resources/views/goal-board.blade.php
+++ resources/views/goal-board.blade.php
@@ -3,165 +3,60 @@
 @section('title', 'Goal Board')
 
 @php
-    $highlighted_page = 'goals';
+$highlighted_page = 'goals';
 @endphp
 
 @section('content')
-<main id="main" class="main">
-    @include('goal-board._page-title')
+    <main id="main" class="main">
+        @include('goal-board._page-title')
 
-    <section class="section dashboard">
-        @include('goal-board._add-goal')
+        <section class="section dashboard">
+            @include('goal-board._add-goal')
 
-        <div class="row">
-            @include('goal-board._goal-card')
-        </div> <!-- / .row -->
-    </section>
-</main>
+            <div class="row">
+                @include('goal-board._goal-card')
+            </div> <!-- / .row -->
+        </section>
+    </main>
 
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        echarts.init(document.querySelector("#pieChart1")).setOption({
-            tooltip: {
-                trigger: 'item'
-            },
-
-            series: [{
-                name: '',
-                type: 'pie',
-                radius: '70%',
-
-                data: [{
-                    value: 10,
-                    name: 'Not started'
-
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            echarts.init(document.querySelector("#pieChart1")).setOption({
+                tooltip: {
+                    trigger: 'item'
                 },
-                {
-                    value: 50,
-                    name: 'Completed'
 
-                },
-                {
-                    value: 40,
-                    name: 'In Progress'
-                }
-                ],
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                series: [{
+                    name: '',
+                    type: 'pie',
+                    radius: '70%',
+
+                    data: [{
+                            value: 10,
+                            name: 'Not started'
+
+                        },
+                        {
+                            value: 50,
+                            name: 'Completed'
+
+                        },
+                        {
+                            value: 40,
+                            name: 'In Progress'
+                        }
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
                     }
-                }
-            }]
+                }]
+            });
         });
-        echarts.init(document.querySelector("#pieChart2")).setOption({
-            tooltip: {
-                trigger: 'item'
-            },
+    </script>
 
-            series: [{
-                name: '',
-                type: 'pie',
-                radius: '70%',
-
-                data: [{
-                    value: 20,
-                    name: 'Not started'
-
-                },
-                {
-                    value: 70,
-                    name: 'Completed'
-
-                },
-                {
-                    value: 10,
-                    name: 'In Progress'
-                }
-                ],
-
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }]
-        });
-        echarts.init(document.querySelector("#pieChart3")).setOption({
-            tooltip: {
-                trigger: 'item'
-            },
-
-            series: [{
-                name: '',
-                type: 'pie',
-                radius: '70%',
-
-                data: [{
-                    value: 20,
-                    name: 'Not started'
-
-                },
-                {
-                    value: 40,
-                    name: 'Completed'
-
-                },
-                {
-                    value: 40,
-                    name: 'In Progress'
-                }
-                ],
-
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }]
-        });
-        echarts.init(document.querySelector("#pieChart4")).setOption({
-            tooltip: {
-                trigger: 'item'
-            },
-
-            series: [{
-                name: '',
-                type: 'pie',
-                radius: '70%',
-
-                data: [{
-                    value: 20,
-                    name: 'Not started'
-
-                },
-                {
-                    value: 30,
-                    name: 'Completed'
-
-                },
-                {
-                    value: 50,
-                    name: 'In Progress'
-                }
-                ],
-
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }]
-        });
-    });
-</script>
-
-<script src="{{ asset('/js/add_actions.js') }}"></script>
+    <script src="{{ asset('/js/add_actions.js') }}"></script>
 @endsection
