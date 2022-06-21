78096e92dd5941fea8e7d9b23af9d0d93d82f879
alifmazli <alifmazli01@gmail.com>
2022-06-21 22:25:31 +0800

    Added background for login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
@@ -14,8 +15,9 @@
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-    rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet" />
 
     <!-- Vendor CSS Files -->
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
@@ -32,72 +34,79 @@
 
 <body>
     <main>
+        @include('auth.background')
         <div class="container">
-            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <section
+                class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                        <div class="d-flex justify-content-center py-4">
-                            <a href="{{ route('home') }}" class="logo d-flex align-items-center w-auto">
-                            <img src="{{ asset('img/logo.png') }}" alt=""/>
-                            <span class="d-none d-lg-block">Objetivo</span>
-                        </a>
-                    </div>
-                    <!-- End Logo -->
-
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="pt-4 pb-2">
-                                <h5 class="card-title text-center pb-0 fs-4">
-                                    Login to Your Account
-                                </h5>
-                                <p class="text-center small">
-                                    Enter your username & password to login
-                                </p>
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="{{ route('home') }}" class="logo d-flex align-items-center w-auto">
+                                    <img src="{{ asset('img/logo.png') }}" alt="" />
+                                    <span id="objetivo" class="d-none d-lg-block">Objetivo</span>
+                                </a>
                             </div>
+                            <!-- End Logo -->
 
-                            <form method="post" action="{{ route('login') }}" class="row g-3 needs-validation" novalidate>
-                                @csrf
-                                <div class="col-12">
-                                    <label for="yourUsername" class="form-label">Username</label>
-                                    <div class="input-group has-validation">
-                                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                        <input type="text" name="username" class="form-control" id="yourUsername" required />
-                                        <div class="invalid-feedback">Please enter your username.</div>
+                            <div class="card mb-3">
+                                <div class="card-body">
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4">
+                                            Login to Your Account
+                                        </h5>
+                                        <p class="text-center small">
+                                            Enter your username and password
+                                        </p>
                                     </div>
-                                </div> <!-- username .col -->
-                                <div class="col-12">
-                                    <label for="yourPassword" class="form-label">Password</label>
-                                    <input type="password" name="password" class="form-control" id="yourPassword" required />
-                                    <div class="invalid-feedback">Please enter your password!</div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe" />
-                                        <label class="form-check-label" for="rememberMe">Remember me</label>
-                                    </div>
-                                </div>
-                                @error('username')
-                                <div class="col-12">
-                                    <div class="alert alert-danger text-center">
-                                        Invalid username or password
-                                    </div>
-                                </div>
-                                @enderror
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100" type="submit">
-                                        Login
-                                    </button>
-                                </div>
-                                <div class="col-12">
-                                    <p class="small mb-0 text-center">
-                                        Don't have an account? <a href="{{ route('register') }}">Create an account</a>
-                                    </p>
-                                </div>
-                            </form>
-                        </div> <!-- .card-body -->
-                    </div> <!-- .card -->
-                </div> <!-- .container -->
+
+                                    <form method="post" action="{{ route('login') }}"
+                                        class="row g-3 needs-validation" novalidate>
+                                        @csrf
+                                        <div class="col-12">
+                                            <label for="yourUsername" class="form-label">Username</label>
+                                            <div class="input-group has-validation">
+                                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                                <input type="text" name="username" class="form-control"
+                                                    id="yourUsername" required />
+                                                <div class="invalid-feedback">Please enter your username.</div>
+                                            </div>
+                                        </div> <!-- username .col -->
+                                        <div class="col-12">
+                                            <label for="yourPassword" class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control"
+                                                id="yourPassword" required />
+                                            <div class="invalid-feedback">Please enter your password!</div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    value="true" id="rememberMe" />
+                                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                                            </div>
+                                        </div>
+                                        @error('username')
+                                            <div class="col-12">
+                                                <div class="alert alert-danger text-center">
+                                                    Invalid username or password
+                                                </div>
+                                            </div>
+                                        @enderror
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit">
+                                                Login
+                                            </button>
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="small mb-0 text-center">
+                                                Don't have an account? <a href="{{ route('register') }}">Create an
+                                                    account</a>
+                                            </p>
+                                        </div>
+                                    </form>
+                                </div> <!-- .card-body -->
+                            </div> <!-- .card -->
+                        </div> <!-- .container -->
             </section>
         </div> <!-- .container -->
     </main>
@@ -120,4 +129,5 @@
     <!-- Template Main JS File -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
