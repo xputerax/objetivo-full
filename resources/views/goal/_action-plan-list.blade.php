4964c4ba52f8c2197f34bbff23e9c34bfde84e6c
Athirah <athirahlokman23@gmail.com>
2022-06-21 17:35:52 +0800

    Update _action-plan-list.blade.php

--- resources/views/goal/_action-plan-list.blade.php
+++ resources/views/goal/_action-plan-list.blade.php
@@ -158,7 +158,6 @@
         <!-- action plan modal -->
         <div class="col-4" style="padding-left: 0px;">
             
-            
 
             <!-- Add action plan modal -->
             <div class="modal fade" id="addActionPlanModal" tabindex="-1">
@@ -226,11 +225,11 @@
                 </div> <!-- / .modal-dialog -->
             </div> <!-- / .modal -->
 
-            <!-- Edit action plan modal -->
+            <!-- Edit action plan modal 
             <div class="modal fade" id="editActionPlanModal" tabindex="-1">
                 <div class="modal-dialog modal-dialog-centered modal-lg">
                     <div class="modal-content">
-                    <!-- edit route here -->
+                    edit route here 
                         <form action="{{ route('action-plan.update', $actionPlan['id']) }}" method="post">
                         @csrf
                         @method('put')
@@ -238,8 +237,8 @@
                             <div class="modal-header">
                                 <h5 class="modal-title"><strong>Edit Action Plan</strong></h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div> <!-- / .modal-header -->
+                                    aria-label="Close"></button>-->
+                           <!-- </div>  / .modal-header
                             <div class="modal-body">
                                 <div class="mb-3">
                                     <label for="inputNanme4" class="form-label">
@@ -247,7 +246,7 @@
                                     </label>
                                     <input type="text" class="form-control" name="title" value="{{ old('title') ?? $actionPlan->title }}">
                                 </div>
-
+                                
                                 <div class="row">
                                     <div class="col-6">
                                         <label for="message-text" class="col-form-label">
@@ -262,20 +261,20 @@
                                         <input type="date" class="form-control" name="end_at" value="{{ old('end_at', date('Y-m-d')) ?? $actionPlan->end_at }}">
                                     </div>
                                 </div>
-                                <input type="hidden" value="{{ $goal->id }} " name="goal_id"/>
-                            </div> <!-- / .modal-body -->
+                                <input type="hidden" value="{{ $goal->id }} " name="goal_id"/> -->
+                            <!--</div>  / .modal-body
 
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                                     Close
                                 </button>
-                                <button type="submit" class="btn btn-outline-primary">Save Changes</button>
-                            </div> <!-- / .modal-footer -->
+                                <button type="submit" class="btn btn-outline-primary">Save Changes</button> -->
+                            <!--</div>  / .modal-footer -->
 
-                        </form>
-                    </div> <!-- / .modal-content -->
-                </div> <!-- / .modal-dialog -->
-            </div> <!-- / .modal -->
+                        <!--</form>-->
+                  <!--  </div>  / .modal-content -->
+                <!-- </div> / .modal-dialog -->
+            <!-- </div> / .modal -->
         </div> <!-- / .col -->
     </div> <!-- / .row -->
   </section>
