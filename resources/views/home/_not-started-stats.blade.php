0981eea5d507c2f267c120b36b593adb5d3968ab
alifmazli <alifmazli01@gmail.com>
2022-06-22 23:03:57 +0800

    Minor reformatting

--- resources/views/home/_not-started-stats.blade.php
+++ resources/views/home/_not-started-stats.blade.php
@@ -1,24 +1,10 @@
 <!-- Not Started Card -->
 <div class="col-xxl-4 col-md-4">
     <div class="card info-card revenue-card">
-        {{-- <div class="filter">
-            <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-            </ul>
-        </div> --}}
-
         <div class="card-body">
             <h5 class="card-title">
                 Not Started<br />
             </h5>
-
             <div class="d-flex align-items-center">
                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                     <i class="bi bi-exclamation-lg"></i>
