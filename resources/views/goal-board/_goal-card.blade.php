356058491790825bf7ae54f0259c4006e24816a5
sarahatiqah <sarahatiqahamram@gmail.com>
2022-06-24 15:39:08 +0800

    commit

--- resources/views/goal-board/_goal-card.blade.php
+++ resources/views/goal-board/_goal-card.blade.php
@@ -143,7 +143,7 @@ use App\Services\GoalService;
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Deleting an Goal</h5>
+                <h5 class="modal-title">Deleting a Goal</h5>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
